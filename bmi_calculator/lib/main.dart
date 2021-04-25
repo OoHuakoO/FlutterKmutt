11841e9f76fbb6554de4a574922305805bcd9d6c
huak <komsark_@hotmail.co.th>
2021-04-25 23:11:14 +0700

    commit

---
+++ bmi_calculator/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My BMI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyMainPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyMainPage extends StatefulWidget {
+  MyMainPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyMainPageState createState() => _MyMainPageState();
+}
+
+class _MyMainPageState extends State<MyMainPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'xxxxx',
+            ),
+            Text(
+              'xxxxx',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
